53e23f84d1ee3416441374cef24dd24b49276ec2
EmilenyRochaLeal <emilenyleal20@gmail.com>
2024-12-23 19:30:08 -0300

    feat: Criação das migrates

---
+++ prisma/migrations/20241223222655_create_table_users/migration.sql
@@ -0,0 +1,15 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('PRODUCER', 'CONSUMER');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Role" NOT NULL DEFAULT 'CONSUMER',
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "update_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
