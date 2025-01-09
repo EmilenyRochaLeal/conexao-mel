5034789c26c7a3f4abc0ff5dac364700f4b3aadc
lealkedna <capic.2021218minf0175@aluno.ifpi.edu.br>
2025-01-09 09:54:05 -0300

    teste

--- src/server.ts
+++ src/server.ts
@@ -23,5 +23,6 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction)=>{
 });
 
 app.listen(3333, ()=>{
-    console.log('Servidor rodando')
+    console.log('Servidor rodando');
+    console.log('adicionei um console');
 })
