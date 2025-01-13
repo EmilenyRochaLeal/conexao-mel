a08695ca81fa5b5c0d8f89fbdf9f37a69435f1c8
AlcilenySilva <capic.2023118tads0045@aluno.ifpi.edu.br>
2025-01-13 20:01:25 -0300

    teste repositorio

--- src/routes.ts
+++ src/routes.ts
@@ -5,7 +5,8 @@ const router = Router();
 
 router.get('/teste', (req: Request, res: Response) => {
     // throw new Error('Erro ao fazer requisição')
-    return res.json({ Nome: 'Emileny' });
+    //return res.json({ Nome: 'Emileny' }); 
+    return res.json({ Nome: 'teste alcilene' });
 });
 
 export { router };
