3e8d8a87e7b317bfb7c95a3c98469daeaae970b2
EmilenyRochaLeal <emilenyleal20@gmail.com>
2025-02-03 10:49:37 -0300

    rota teste

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express';
 import { CreateUserController } from './controller/user/CreateUserController';
-import { AuthUserController } from './controller/user/AuthUserController';
 
 const router = Router();
 
 // Rotas user
 router.post('/users', new CreateUserController().handle)
-router.post('/signin', new AuthUserController().handle)
 
 // Rota para testar o deploy backend 
 router.get('/', (req, res) => {
