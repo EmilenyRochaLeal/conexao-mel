098f3a7c1db189f30439932c88568c3ba2ea0bad
lealkedna <capic.2021218minf0175@aluno.ifpi.edu.br>
2025-02-20 15:05:49 -0300

    feact: implementa validações

---
+++ src/router.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { CreateUserController } from "./controller/user/CreateUserController";
+
+
+const router = Router();
+
+const createUserController = new CreateUserController();
+
+// Rotas user
+// router.post('/users', new CreateUserController().handle)
+
+// Rota para testar o deploy backend
+router.get("/", (req, res) => {
+    res.send("Conexão Mel está funcionando!");
+  });
+  
+
+router.post("/register", (req, res) => createUserController.handle(req, res));
+
+export default  router;
--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -1,41 +1,76 @@
+import { PrismaClient } from "@prisma/client";
 import prismaClient from "../../prisma/indext";
 import { hash } from "bcryptjs";
+import bcrypt from "bcryptjs";
 
-interface UserRequest{
-    name: string,
-    email: string,
-    password: string
+const prisma = new PrismaClient();
+
+interface UserRequest {
+  name: string;
+  email: string;
+  password: string;
 }
-class CreateUserService{
-    async execute({name, email, password}: UserRequest){
-        
-        if (!email){
-            throw new Error('Email incorreto')
-        }
-        const userExists = await prismaClient.user.findFirst({
-            where:{
-                email: email
-            }
-        })
-        if (userExists){
-            throw new Error("Usuário já existe");
-        }
 
-        const passwordHash = await hash(password, 8);
-        const user = await prismaClient.user.create({
-            data: {
-                name: name,
-                email: email,
-                password: passwordHash,
-            },
-            select: {
-                id: true,
-                name: true,
-                email: true
-            }
-        })
-        return user;
+// Função para validar senha segura
+const isPasswordValid = (password: string): boolean => {
+  const regex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  return regex.test(password);
+};
+
+// Serviço de criação de usuário
+export class CreateUserService {
+  async execute({
+    name,
+    email,
+    password,
+    role,
+  }: {
+    name: string;
+    email: string;
+    password: string;
+    role?: string;
+  }) {
+    //Verificação se o e-mailjá está cadastrado
+    const existeEmail = await prisma.user.findUnique({ where: { email } });
+    if (!existeEmail) {
+      throw new Error("Email já cadastrado!");
     }
+
+    const userExists = await prismaClient.user.findFirst({
+      where: {
+        email: email,
+      },
+    });
+    if (userExists) {
+      throw new Error("Usuário já existe");
+    }
+
+    // Para Validar a senha
+    if (!isPasswordValid(password)) {
+      throw new Error(
+        "A senha deve ter pelo menos 8 caracteres, incluindo uma letra maiúscula, um número e um caractere especial."
+      );
+    }
+
+    // Hash da senha antes de salvar
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    // const passwordHash = await hash(password, 8);
+
+    const user = await prismaClient.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: passwordHash,
+        role: "VENDEDOR",
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      },
+    });
+    return user;
+  }
 }
 
-export { CreateUserService };
