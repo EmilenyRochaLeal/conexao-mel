af682fd508de9a3f4fc071325cb1925fc9fcbea2
lealkedna <capic.2021218minf0175@aluno.ifpi.edu.br>
2025-02-21 19:37:10 -0300

    feat:Revisa a estrutura do banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
 import express, {Response, Request, NextFunction} from 'express'
 import 'express-async-errors';
 import cors from 'cors';
-
-import {router} from './routes'
+import router from './router'
 
 const app = express();
 
-app.use(express.json());
+// Permite o uso de json
+app.use(express.json()); 
+
 app.use(cors());
+
 app.use(router);
+
+// Middleware para tratamento de erros
 app.use((err: Error, req: Request, res: Response, next: NextFunction)=>{
     if (err instanceof Error){
         return res.status(400).json({
@@ -22,6 +26,9 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction)=>{
     })
 });
 
+const PORT = process.env.PORT || 3333;
+
 app.listen(3333, ()=>{
-    console.log('Servidor rodando');
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+    
 })
